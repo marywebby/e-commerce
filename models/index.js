1db0e0e3a74c5d07abdf30d6004a1dfc013d89e4
Mary Webby <MaryWebby@Marys-MacBook-Air-3.local>
2023-03-05 16:23:07 -0500

    defined columns and created associations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// BELONGSTO DOES NOT REQUIRE A CASCADE DELETE
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_ids',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_ids',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+});
 
 module.exports = {
   Product,
